9c9c3f2f0ad0409323c674b5fa3aa624ca49aa82
Alexandre Loumi <alexandreloumi1995@gmail.com>
2024-09-16 12:48:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// /////// MAKING THE ANIMATION ON 'AND I MAKE...' 
+
+// const jobs = ['websites', 'apps', 'dashboards', 'ecommerces'];
+// const colors = ['blue', '#FC4F4F', '#ADFC92', '#EF1A68']
+// let jobIndex = 0;
+
+// const changeJobElement = document.querySelector('.job');
+
+// function changeJob() {
+//     jobIndex = (jobIndex + 1) % jobs.length;
+//     changeJobElement.textContent = jobs[jobIndex];
+
+//     // Change the background color
+//     const newColor = colors[jobIndex];
+//     changeJobElement.style.backgroundColor = newColor;
+
+//     if(newColor === 'blue' || newColor === '#FC4F4F' || newColor === '#EF1A68') {
+//         changeJobElement.style.color = '#FFFFFF'
+//     } else {
+//         changeJobElement.style.color = '';
+//     }
+
+// }
+
+// setInterval(changeJob, 1000);
